229e9ca96fd5132ba99e73712848fc7f0691d017
Jubelo <ige.oluwasegun@yahoo.co.uk>
2019-11-08 14:05:10 +0100

    error message

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
                 $table->string('role');
+                $table->integer('isBlocked')->default(0);
                 $table->rememberToken();
                 $table->timestamps();
                 $table->softDeletes();
